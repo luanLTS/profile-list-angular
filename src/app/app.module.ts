887555265e937ab9f20664dbca77114020b454b4
Luan Teixeira <luanlts@outlook.com>
2022-04-15 19:58:36 -0300

    terminado o card de perfil, iteracao para listar e o formulário para adicionar novos perfis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BannerProfileComponent } from './banner-profile/banner-profile.component';
@@ -8,7 +8,7 @@ import { ProfileListComponent } from './profile-list/profile-list.component';
 
 @NgModule({
   declarations: [AppComponent, BannerProfileComponent, ProfileListComponent],
-  imports: [BrowserModule, NgModel],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
