3b6f3096d5f6e383047617210b0dd4eebca357b1
Luan Teixeira <luanlts@outlook.com>
2022-04-20 15:59:51 -0300

    gerado o componente de modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BannerProfileComponent } from './banner-profile/banner-profile.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, BannerProfileComponent, ProfileListComponent],
+  declarations: [AppComponent, BannerProfileComponent, ProfileListComponent, ModalComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
